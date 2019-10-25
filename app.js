2fc56e7ee253b1ae7c918df348a49cc658ac49d6
Robert <rrami007@gmail.com>
2019-10-25 10:06:50 -0400

    setup backend

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
