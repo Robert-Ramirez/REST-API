c5c7d0cec3e0b9e1bd8771aba724512347ecd4c1
Robert <rrami007@gmail.com>
2019-10-26 22:09:23 -0400

    api

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const tasks = JSON.parse(fs.readFileSync(`${__dirname}/data/tasks.json`));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(methodOverride("_method"));
+//handles json
+app.use(express.json());
 
-var id = 2;
-var tasks = [ {id: 0, name: "school"}, {id: 1, name: "test"} ];
-
-
-app.get("/", function(req, res){
-    res.render("tasks", {tasks:tasks});
+//routers
+app.get("/api/v0/tasks/", (req, res) => {
+  res.status(200).json({
+    data: {
+      tasks
+    }
+  });
 });
 
-app.get("/tasks", function(req, res){
-    res.render("tasks", {tasks:tasks});
+app.get("/api/v0/tasks/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const task = tasks.find(el => el.id === id);
+  res.status(200).json({
+    data: {
+      task
+    }
+  });
 });
 
-// CREATE Task
-app.post("/tasks", function(req, res){
-    // get data from form and add to tasks array
-    var name = req.body.name;
-    var newTask = {id:id, name: name};
-    id++;
-    tasks.push(newTask);
-    //redirect back to tasks page
-    res.redirect("/tasks");
+app.post("/api/v0/tasks/", (req, res) => {
+  const newId = tasks[tasks.length - 1].id + 1;
+  const newTask = Object.assign({ id: newId }, req.body);
+  tasks.push(newTask);
+  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(tasks), err => {
+    res.status(201).json();
+  });
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.patch("/api/v0/tasks/:id", (req, res) => {
+  const id = req.params.id * 1;
+  let task = tasks.filter(el => el.id !== id);
+  const newTask = Object.assign({ id: id }, req.body);
+  task.push(newTask);
+  task.sort((a, b) => {
+    return a.id - b.id;
+  });
+  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(task), err => {
+    res.status(201).json();
+  });
+});
+
+app.delete("/api/v0/tasks/:id", (req, res) => {
+  const id = req.params.id * 1;
+  let task = tasks.filter(el => el.id !== id);
+  task.sort((a, b) => {
+    return a.id - b.id;
+  });
+  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(task), err => {
+    res.status(204).json();
+  });
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- public/js/todos.js
+++
@@ -1,8 +0,0 @@
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('UL');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-      console.log(ev);
-	  ev.target.classList.toggle('checked');
-  }
-}, false);
