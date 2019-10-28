e78e91455d9d048e5fe64c7ce96b7458c5156f56
Robert <rrami007@gmail.com>
2019-10-28 09:22:57 -0400

    refactor routes

--- app.js
+++ app.js
@@ -1,62 +1,17 @@
-const fs = require("fs");
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+
+const taskRouter = require('./routes/taskRouter');
+
 const app = express();
-const tasks = JSON.parse(fs.readFileSync(`${__dirname}/data/tasks.json`));
 
-//handles json
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-//routers
-app.get("/api/v0/tasks/", (req, res) => {
-  res.status(200).json({
-    data: {
-      tasks
-    }
-  });
-});
+app.use('/api/v1/tasks', taskRouter);
 
-app.get("/api/v0/tasks/:id", (req, res) => {
-  const id = req.params.id * 1;
-  const task = tasks.find(el => el.id === id);
-  res.status(200).json({
-    data: {
-      task
-    }
-  });
-});
-
-app.post("/api/v0/tasks/", (req, res) => {
-  const newId = tasks[tasks.length - 1].id + 1;
-  const newTask = Object.assign({ id: newId }, req.body);
-  tasks.push(newTask);
-  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(tasks), err => {
-    res.status(201).json();
-  });
-});
-
-app.patch("/api/v0/tasks/:id", (req, res) => {
-  const id = req.params.id * 1;
-  let task = tasks.filter(el => el.id !== id);
-  const newTask = Object.assign({ id: id }, req.body);
-  task.push(newTask);
-  task.sort((a, b) => {
-    return a.id - b.id;
-  });
-  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(task), err => {
-    res.status(201).json();
-  });
-});
-
-app.delete("/api/v0/tasks/:id", (req, res) => {
-  const id = req.params.id * 1;
-  let task = tasks.filter(el => el.id !== id);
-  task.sort((a, b) => {
-    return a.id - b.id;
-  });
-  fs.writeFile(`${__dirname}/data/tasks.json`, JSON.stringify(task), err => {
-    res.status(204).json();
-  });
-});
-
-const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app;
