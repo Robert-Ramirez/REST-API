9191b8e62aa9d730c2b065bfa1fe0a6ace599883
Robert <rrami007@gmail.com>
2019-10-28 11:01:17 -0400

    saved

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -65,10 +65,7 @@ exports.deleteTask = async (req, res) => {
   try {
     await Task.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
+    res.status(204).json();
   } catch (err) {
     res.status(404).json({
       message: err
